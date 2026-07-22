7239a61c8c1d0c1a84f4a0b8a98c3a65d0a8551f
Wasike Onesmus <wasikeonesmus980@gmail.com>
2026-07-22 23:46:58 +0300

    Fix Docker container Errno 101 Network is unreachable by introducing IPv4EmailBackend

---
+++ scheduler/email_backend.py
@@ -0,0 +1,82 @@
+import socket
+import smtplib
+from django.core.mail.backends.smtp import EmailBackend
+
+class IPv4SMTPConnection(smtplib.SMTP_SSL):
+    """SMTP_SSL subclass forcing IPv4 (socket.AF_INET) resolution for Docker compatibility."""
+    def _get_socket(self, host, port, timeout):
+        infos = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+        err = None
+        for res in infos:
+            af, socktype, proto, canonname, sa = res
+            sock = None
+            try:
+                sock = socket.socket(af, socktype, proto)
+                if timeout is not None:
+                    sock.settimeout(timeout)
+                if self.source_address:
+                    sock.bind(self.source_address)
+                sock.connect(sa)
+                return sock
+            except socket.error as e:
+                err = e
+                if sock is not None:
+                    sock.close()
+        if err is not None:
+            raise err
+        else:
+            raise socket.error("getaddrinfo returned empty list")
+
+
+class IPv4SMTPTLSConnection(smtplib.SMTP):
+    """SMTP subclass forcing IPv4 (socket.AF_INET) resolution for Docker compatibility."""
+    def _get_socket(self, host, port, timeout):
+        infos = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+        err = None
+        for res in infos:
+            af, socktype, proto, canonname, sa = res
+            sock = None
+            try:
+                sock = socket.socket(af, socktype, proto)
+                if timeout is not None:
+                    sock.settimeout(timeout)
+                if self.source_address:
+                    sock.bind(self.source_address)
+                sock.connect(sa)
+                return sock
+            except socket.error as e:
+                err = e
+                if sock is not None:
+                    sock.close()
+        if err is not None:
+            raise err
+        else:
+            raise socket.error("getaddrinfo returned empty list")
+
+
+class IPv4EmailBackend(EmailBackend):
+    """
+    Custom Django EmailBackend that forces IPv4 connections.
+    Prevents 'OSError: [Errno 101] Network is unreachable' caused by
+    Docker container bridge networks trying unrouted IPv6 addresses.
+    """
+    def open(self):
+        if self.connection:
+            return False
+        connection_class = IPv4SMTPConnection if self.use_ssl else IPv4SMTPTLSConnection
+        try:
+            self.connection = connection_class(
+                self.host,
+                self.port,
+                timeout=self.timeout,
+                source_address=self.source_address,
+            )
+            if not self.use_ssl and self.use_tls:
+                self.connection.starttls()
+            if self.username and self.password:
+                self.connection.login(self.username, self.password)
+            return True
+        except Exception:
+            if not self.fail_silently:
+                raise
+            return False
